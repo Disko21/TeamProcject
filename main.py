cde909b1cec5004e6ae0d201c0f4b33810a52382
Piotr Rebizant <48749136+Disko21@users.noreply.github.com>
2023-06-21 23:36:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,194 +1,254 @@
-import glob
-import math
-import os
 import sys
-from collections import namedtuple
-from os.path import join
+import tkinter as tk
+from PyQt5.QtWidgets import QDialog,QApplication, QWidget, QPushButton, QLineEdit, QLabel, QGridLayout, QFileDialog,QMainWindow,QVBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from os import listdir
+from os.path import isfile, join
+from exif import Image,DATETIME_STR_FORMAT
+import exifread
+import os
+import ast
+from fractions import Fraction
+from datetime import datetime
 
-from PyQt5.QtCore import QAbstractTableModel, Qt, QSize, QItemSelectionModel
-from PyQt5.QtGui import QImage
-from PyQt5.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QTableView,
-    QStyledItemDelegate,
-    QPushButton,
-    QLabel,
-    QFileDialog,
-    QMessageBox,
-    QStyle,
-)
-
-from exif import Image
-
-# Create a custom namedtuple class to hold our data
-preview = namedtuple("preview", "id title image exif_data")
-
-NUMBER_OF_COLUMNS = 4
-CELL_PADDING = 20
-
-
-class PreviewDelegate(QStyledItemDelegate):
-    def __init__(self, view):
-        super().__init__(view)
-        self.view = view
-        self.open_windows = []
-
-    def paint(self, painter, option, index):
-        print("paint")
-        data = index.model().data(index, Qt.DisplayRole)
-        if data is None:
-            return
-
-        width = option.rect.width() - CELL_PADDING * 2
-        height = option.rect.height() - CELL_PADDING * 2
-
-        scaled = data.image.scaled(
-            width,
-            height,
-            aspectRatioMode=Qt.KeepAspectRatio,
-        )
-        x = int(CELL_PADDING + (width - scaled.width()) / 2)
-        y = int(CELL_PADDING + (height - scaled.height()) / 2)
-
-        painter.drawImage(option.rect.x() + x, option.rect.y() + y, scaled)
-
-        if option.state & QStyle.State_Selected:
-            painter.setPen(Qt.red)
-            painter.drawRect(option.rect)
-
-            exif_data = index.data(Qt.UserRole)
-            if exif_data:
-                print("open window")
-                self.open_new_window(exif_data, index.row(), index.column())
-        # else:
-        #     self.close_windows()
-
-    def open_new_window(self, exif_data, row, column):
-        new_window = QMainWindow()
-        new_window.setWindowTitle("Metadata")
-        new_window.setGeometry(100, 100, 400, 300)
-
-        label = QLabel(exif_data, new_window)
-        label.setGeometry(10, 10, 380, 280)
-        label.setWordWrap(True)
-
-        self.open_windows.append(new_window)
-
-        new_window.show()
-        self.w = new_window
-        # Deselect the image
-        model_index = self.view.model().index(row, column)
-        self.view.selectionModel().select(model_index, QItemSelectionModel.Deselect)
-
-    # def close_windows(self):
-    #     #print("close windows")
-    #     for window in self.open_windows:
-    #         window.close()
-    #     self.open_windows = []
-
-    def sizeHint(self, option, index):
-        return QSize(300, 200)
-
-
-class PreviewModel(QAbstractTableModel):
-    def __init__(self):
-        super().__init__()
-        self.previews = []
-
-    def data(self, index, role):
-        try:
-            data = self.previews[index.row() * 4 + index.column()]
-        except IndexError:
-            return
-
-        if role == Qt.DisplayRole:
-            return data
-
-        if role == Qt.ToolTipRole:
-            return data.title
-
-        if role == Qt.UserRole:
-            return data.exif_data
-
-    def columnCount(self, index):
-        return NUMBER_OF_COLUMNS
-
-    def rowCount(self, index):
-        n_items = len(self.previews)
-        return math.ceil(n_items / NUMBER_OF_COLUMNS)
-
-
-class Window2(QMainWindow):
-    def __init__(self, directory_path):
-        super().__init__()
-        self.setFixedSize(1230, 660)
-        self.setWindowTitle("PATH: %s" % directory_path)
-        self.view = QTableView()
-        self.view.horizontalHeader().hide()
-        self.view.verticalHeader().hide()
-        self.view.setGridStyle(Qt.NoPen)
-
-        delegate = PreviewDelegate(self.view)
-        self.view.setItemDelegate(delegate)
-        self.model = PreviewModel()
-        self.view.setModel(self.model)
-
-        self.setCentralWidget(self.view)
-
-        for n, fn in enumerate(glob.glob(join(directory_path, "*.jpg"))):
-            if not os.path.isfile(fn):
-                continue
-
-            with open(fn, "rb") as f:
-                my_image = Image(f)
-                if not my_image.has_exif:
-                    continue
-                else:
-                    image = QImage(fn)
-                    exif_data = str(my_image)
-                    item = preview(n, fn, image, exif_data)
-                    self.model.previews.append(item)
-        self.model.layoutChanged.emit()
-
-        self.view.resizeRowsToContents()
-        self.view.resizeColumnsToContents()
-
-    def closeEvent(self, event):
-        delegate = self.view.itemDelegate()
-        delegate.close_new_window()
-        self.view.clearSelection()
-        event.accept()
-
-class Window(QMainWindow):
+def openDirectory():
+    directory_path = QFileDialog.getExistingDirectory()
+    # print all files from directory
+    all_files = [directory_path + "/" + f for f in listdir(directory_path) if isfile(directory_path + "/" + f) and f.lower().endswith(('.jpeg','.jpg'))]
+    return all_files
+class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.setWindowTitle('Edytor zdjęć')
 
-        self.title = "Test 0.1.0v"
-        self.top = 100
-        self.left = 100
-        self.width = 680
-        self.height = 500
+        # set the grid layout
+        layout = QGridLayout()
+        self.setFixedSize(300, 220)
+        self.setLayout(layout)
+        
+        # button
+        b2 = QPushButton('Start')
 
-        self.pushButton = QPushButton("Start", self)
-        self.pushButton.move(275, 200)
-        self.pushButton.clicked.connect(self.main_window)
+        
+        layout.addWidget(b2,3,0, alignment=Qt.AlignmentFlag.AlignCenter)
+        # button onclick event
+        b2.clicked.connect(self.show_new_window)
+    def show_new_window(self):
+        new_window = NewWindow()
+        new_window.exec_()
 
-        self.start_window()
+class NewWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Nowe okno")
+        layout1 = QGridLayout()
+        self.setLayout(layout1)
+        
+        tab1=openDirectory()
+        print(tab1)
 
-    def start_window(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.top, self.left, self.width, self.height)
-        self.show()
+        button = QPushButton("następny obrazek")
+        button2 = QPushButton("edycja metadany")
+        button3 = QPushButton("cofnij obrazek")
+        label = QLabel()
+        pixmap = QPixmap(tab1[0])
+        scaled_pixmap = pixmap.scaled(600, 400)
+        label.setPixmap(scaled_pixmap)
+        layout1.addWidget(button3,4,1, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout1.addWidget(button, 4,2, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout1.addWidget(label,4,0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout1.addWidget(button2,4,3, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.b=1
+        def change_image():
+            pixmap2 = QPixmap(tab1[self.b])
+            scaled_pixmap2 = pixmap2.scaled(600, 400)
+            label.setPixmap(scaled_pixmap2)
+            #print(self.b)
+            self.b +=1
+            if self.b==len(tab1):
+                self.b=0;
 
-    def main_window(self):
-        directory_path = QFileDialog.getExistingDirectory()
-        if directory_path:
-            self.w = Window2(directory_path)
-            self.w.show()
+        def change_image2():
+            pixmap2 = QPixmap(tab1[self.b])
+            scaled_pixmap2 = pixmap2.scaled(600, 400)
+            label.setPixmap(scaled_pixmap2)
+            #print(self.b)
+            if self.b==0:
+                self.b=len(tab1);
+            self.b -=1
+
+        def open_new_window(event):
+            new_window = NewWindow2(tab1,self.b)
+            new_window.exec_()
+
+        def open_new_window2():
+            new_window = NewWindow3(tab1,self.b)
+            new_window.exec_()
 
 
-if __name__ == "__main__":
+        button3.clicked.connect(change_image2)
+        button.clicked.connect(change_image)
+        label.mousePressEvent = open_new_window
+        button2.clicked.connect(open_new_window2)
+        self.show()  
+class NewWindow2(QDialog):
+    def __init__(self, tab1, b):
+        super().__init__()
+        filename = os.path.basename(tab1[b-1])
+        self.setWindowTitle(filename)
+        self.tab1 = tab1
+        self.b = b
+        layout = QGridLayout()
+        self.setLayout(layout)
+        
+        label = QLabel()
+        pixmap = QPixmap(self.tab1[b-1])
+        scaled_pixmap = pixmap.scaled(600, 400)
+        label.setPixmap(scaled_pixmap)
+
+        with open(tab1[b-1], 'rb') as image_file:
+            tags = exifread.process_file(image_file, details=False)
+            tag_keys = tags.keys()
+            print(tags)
+         
+
+        label1 = QLabel()   
+        label1.setText("Tagi:")
+        layout.addWidget(label1, 3, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(label, 1, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        row = 5
+        col = 0
+        col_span = 2
+
+        #addWidget TAGKEY
+        for tag_key, tag_value in tags.items():
+            tag_label = QLabel()
+            tag_label.setText(f"{tag_key}: {tag_value}")
+            layout.addWidget(tag_label, row, col, 1, col_span, alignment=Qt.AlignmentFlag.AlignLeft)
+
+            col += col_span
+            if col >= 4:
+                col = 0
+                row += 1
+
+        layout.addWidget(label, row+1, 0, 1, 4, alignment=Qt.AlignmentFlag.AlignCenter)
+        
+        self.setMinimumSize(600, 400)
+class NewWindow3(QDialog):
+    def __init__(self, tab1, b):
+        super().__init__()
+
+        self.setMinimumSize(600, 400)
+
+        filename = os.path.basename(tab1[b-1])
+        self.setWindowTitle(filename)
+
+        self.tab1 = tab1
+
+        layout = QGridLayout()
+        self.setLayout(layout)
+   
+
+
+        with open(tab1[b-1], 'rb') as image_file:
+            my_image = Image(image_file)
+            tab2=my_image.list_all()
+            
+        
+
+        def zast():
+               model=metadana.text()
+
+               nazwa=metadana2.text()
+
+               gps=metadana3.text()
+               gps=gps.split(',')
+
+               gps2=metadana6.text()
+               gps2=gps2.split(',')
+
+               data=metadana4.text()
+               data=data.split('.')
+               data_wykonania = datetime(year=int(data[0]), month=int(data[1]), day=int(data[2]), hour=int(data[3]), minute=int(data[4]), second=int(data[5]))
+               my_image.datetime_original=data_wykonania.strftime(DATETIME_STR_FORMAT)
+               
+               data2=metadana5.text()
+               data2=data2.split('.')
+               data_s= datetime(year=int(data2[0]), month=int(data2[1]), day=int(data2[2]), hour=int(data2[3]), minute=int(data2[4]), second=int(data2[5]))
+               my_image.datetime_digitized=data_s.strftime(DATETIME_STR_FORMAT)
+               
+               print(f"{gps}+{nazwa}+{model}+{data_wykonania}")
+               filename=nazwa
+               my_image.gps_longitude=(float(gps2[0]),float(gps2[1]),float(gps2[2]))
+               my_image.gps_latitude=(float(gps[0]),float(gps[1]),float(gps[2]))
+               my_image.model=model
+
+               with open(nazwa, 'wb') as new_image_file:
+                    new_image_file.write(my_image.get_file())
+
+        metadana=QLineEdit()
+        label = QLabel()
+        label.setText("Model_urządzenia")
+        
+        metadana2=QLineEdit()
+        label2= QLabel()
+        label2.setText("Nazwa pliku")
+        
+
+        metadana3=QLineEdit()
+        label3= QLabel()
+        label3.setText("gps_latitude(-.-,-.-,-.-)")
+
+        metadana4=QLineEdit()
+        label4= QLabel()
+        label4.setText("Data_Wykonania(yyyy.mm.dd.hh.min.s)")
+
+        metadana5=QLineEdit()
+        label5= QLabel()
+        label5.setText("Data_S(yyyy.mm.dd.hh.min.s)")
+
+        metadana6=QLineEdit()
+        label6= QLabel()
+        label6.setText("gps_longitude(-.-,-.-,-.-)")
+
+
+        zastosuj = QPushButton("Zastosuj")
+        zastosuj.clicked.connect(zast)
+
+
+          
+        zastosuj.clicked.connect(zast)  
+        layout.addWidget(label, 1, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(metadana, 1, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(label2, 2, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(metadana2, 2, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(label3, 3, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(metadana3, 3, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(label4, 4, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(metadana4, 4, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(label5, 5, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(metadana5, 5, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(label6, 6, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(metadana6, 6, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addWidget(zastosuj, 7,1, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        
+
+
+if __name__ == '__main__':
+
+    # create pyqt5 app
     app = QApplication(sys.argv)
-    window = Window()
+    # create the instance of our Window
+    window = MainWindow()
+    window.show()
+
+    # start the app
     sys.exit(app.exec())
